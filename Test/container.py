cfd1e2f306fc39d023643c9d894aa246fe633218
Francesco Minna <fra1995m@gmail.com>
2021-11-18 17:59:56 +0100

    Delete Test directory

--- Test/container.py
+++
@@ -1,9 +0,0 @@
-
-class Container:
-    
-    def __init__(self, id, name, registry, dockerfile, bundle): 
-        self.id = id
-        self.name = name
-        self.registry = registry
-        self.Dockerfile = dockerfile
-        self.ImageBundle = bundle
