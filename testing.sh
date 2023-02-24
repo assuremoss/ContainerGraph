4501b683e2113da97976359e2641bd4a41697159
Francesco Minna <fra1995m@gmail.com>
2023-02-24 18:37:16 +0100

    Updated testing script

--- testing.sh
+++ testing.sh
@@ -8,7 +8,7 @@ Bash script to execute the ContainerGraph tool on several ($TESTING_SCALE) conta
 
 # Total number of runs to test the tool
 # For example, we test 10 containers 25 times, retrieving the min, avg, and max execution times.
-export TOTAL_RUNS=25
+export TOTAL_RUNS=5
 
 # Log file to save execution times
 export LOG_FILE=execution_times.log
@@ -32,12 +32,8 @@ echo "" >> $LOG_FILE
 echo "Testing 1 container"
 
 echo "Cleaning up the database..."
-# 1. Clean-up database and remove all containers
+# Clean-up database and remove all containers
 python main.py --remove all
-
-# 2. Initialize the database (configuration + vulnerabilities)
-echo "Initializing the database..."
-python main.py --run ciao 
 
 echo "Execution Times for 1 privileged Docker container"
 
@@ -46,18 +42,26 @@ echo "" >> $LOG_FILE
 
 for i in $(seq 1 $TOTAL_RUNS); do 
 
-    # 3. Computing Loading Time
-    echo " > $i Loading Time" >> $LOG_FILE
-    /usr/bin/time -a -o $LOG_FILE -p python main.py --run docker run -it --rm -d --privileged nginx >/dev/null
-        
-    # 4. Compute Analyzing Time
-    echo " > $i Analyzing Time" >> $LOG_FILE
-    yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
+    python main.py --run ciao >/dev/null
 
-    # 5. Clean-up
-    python main.py --remove containers >/dev/null
-    echo "" >> $LOG_FILE
+    for i in $(seq 1 $TOTAL_RUNS); do 
+ 
+        # Computing Loading Time
+        echo " > $i Loading Time" >> $LOG_FILE
+        /usr/bin/time -a -o $LOG_FILE -p python main.py --run docker run -it --rm -d --privileged nginx >/dev/null
+            
+        # Compute Analyzing Time
+        echo " > $i Analyzing Time" >> $LOG_FILE
+        yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
 
+        # Clean-up
+        python main.py --remove containers >/dev/null
+        echo "" >> $LOG_FILE
+
+    done
+
+    python main.py --remove all
+    
 done
 
 echo "------------" >> $LOG_FILE
@@ -71,12 +75,8 @@ echo "" >> $LOG_FILE
 echo "Testing 10 containers (1 priv. + 9 default)"
 
 echo "Cleaning up the database..."
-# 1. Clean-up database and remove all containers
+# Clean-up database and remove all containers
 python main.py --remove all
-
-# 2. Initialize the database (configuration + vulnerabilities)
-echo "Initializing the database..."
-python main.py --run ciao >/dev/null
 
 echo "Execution Times for 10 containers (1 priv. and 9 default)" >> $LOG_FILE
 echo "" >> $LOG_FILE
@@ -85,23 +85,31 @@ echo "Computing Loading and Analyzing times for 10 containers..."
 
 for i in $(seq 1 $TOTAL_RUNS); do 
 
-    # 3. Computing Loading Time
-    echo " > $i Loading Time" >> $LOG_FILE
-    /usr/bin/time -a -o $LOG_FILE -p sh -c '
-    python main.py --run docker run -it --rm -d --privileged nginx >/dev/null;
-    for i in $(seq 1 9); do 
-        python main.py --run docker run -it --rm -d nginx >/dev/null;
+    python main.py --run ciao >/dev/null
+
+    for i in $(seq 1 $TOTAL_RUNS); do 
+
+        # Computing Loading Time
+        echo " > $i Loading Time" >> $LOG_FILE
+        /usr/bin/time -a -o $LOG_FILE -p sh -c '
+        python main.py --run docker run -it --rm -d --privileged nginx >/dev/null;
+        for i in $(seq 1 9); do 
+            python main.py --run docker run -it --rm -d nginx >/dev/null;
+        done
+        '
+
+        # Compute Analyzing Time
+        echo " > $i Analyzing Time" >> $LOG_FILE
+        yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
+
+        # Clean-up
+        python main.py --remove containers >/dev/null
+        echo "" >> $LOG_FILE
+
     done
-    '
 
-    # 4. Compute Analyzing Time
-    echo " > $i Analyzing Time" >> $LOG_FILE
-    yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
-
-    # 5. Clean-up
-    python main.py --remove containers >/dev/null
-    echo "" >> $LOG_FILE
-
+    python main.py --remove all
+    
 done
 
 echo "------------" >> $LOG_FILE
@@ -118,10 +126,6 @@ echo "Cleaning up the database..."
 # 1. Clean-up database and remove all containers
 python main.py --remove all
 
-# 2. Initialize the database (configuration + vulnerabilities)
-echo "Initializing the database..."
-python main.py --run ciao >/dev/null
-
 echo "Execution Times for 10 mixed containers" >> $LOG_FILE
 echo "" >> $LOG_FILE
 
@@ -132,34 +136,42 @@ echo "Computing Loading and Analyzing times for 10 containers..."
 
 for i in $(seq 1 $TOTAL_RUNS); do 
 
-    # 1. Container mixed 1 uses 3 of the most common CAPs that we found on GitHub and StackOverflow
-    # 2. Container mixed 2 uses other 3 of the most common CAPs that we found on GitHub and StackOverflow
-    # 3. Container mixed 3 uses a configuration vulnerable to a container escaping attack
-        # https://blog.trailofbits.com/2019/07/19/understanding-docker-container-escapes/
-    # 4. Container mixed 4 uses the AppAmor Nginx profile available in the official Docker documentation
-        # https://docs.docker.com/engine/security/apparmor/
-    # 5. Container mixed 5 uses 3 common recommendations suggested by the Docker CIS Benchmark (i.e., limited cpus and memory)
+    python main.py --run ciao >/dev/null
 
-    # 3. Computing Loading Time
-    echo " > $i Loading Time" >> $LOG_FILE
-    /usr/bin/time -a -o $LOG_FILE -p sh -c '
-    for i in $(seq 1 2); do 
-        python main.py --run docker run -it --rm -d --cap-add NET_ADMIN --cap-add SYS_PTRACE nginx >/dev/null;
-        python main.py --run docker run -it --rm -d --cap-add CAP_IPC_LOCK --cap-add SYS_ADMIN nginx >/dev/null;
-        python main.py --run docker run -it --rm -d --security-opt apparmor=unconfined --cap-add SYS_ADMIN nginx >/dev/null;
-        python main.py --run docker run --security-opt apparmor=docker-nginx --security-opt no-new-privileges -d nginx >/dev/null;
-        python main.py --run docker run -it --rm -d --cpus=2 -m 1GB nginx >/dev/null;
+    for i in $(seq 1 $TOTAL_RUNS); do 
+
+        # 1. Container mixed 1 uses 3 of the most common CAPs that we found on GitHub and StackOverflow
+        # 2. Container mixed 2 uses other 3 of the most common CAPs that we found on GitHub and StackOverflow
+        # 3. Container mixed 3 uses a configuration vulnerable to a container escaping attack
+            # https://blog.trailofbits.com/2019/07/19/understanding-docker-container-escapes/
+        # 4. Container mixed 4 uses the AppAmor Nginx profile available in the official Docker documentation
+            # https://docs.docker.com/engine/security/apparmor/
+        # 5. Container mixed 5 uses 3 common recommendations suggested by the Docker CIS Benchmark (i.e., limited cpus and memory)
+
+        # 3. Computing Loading Time
+        echo " > $i Loading Time" >> $LOG_FILE
+        /usr/bin/time -a -o $LOG_FILE -p sh -c '
+        for i in $(seq 1 2); do 
+            python main.py --run docker run -it --rm -d --cap-add NET_ADMIN --cap-add SYS_PTRACE nginx >/dev/null;
+            python main.py --run docker run -it --rm -d --cap-add CAP_IPC_LOCK --cap-add SYS_ADMIN nginx >/dev/null;
+            python main.py --run docker run -it --rm -d --security-opt apparmor=unconfined --cap-add SYS_ADMIN nginx >/dev/null;
+            python main.py --run docker run --security-opt apparmor=docker-nginx --security-opt no-new-privileges -d nginx >/dev/null;
+            python main.py --run docker run -it --rm -d --cpus=2 -m 1GB nginx >/dev/null;
+        done
+        '
+
+        # 4. Compute Analyzing Time
+        echo " > $i Analyzing Time" >> $LOG_FILE
+        yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
+
+        # 5. Clean-up
+        python main.py --remove containers >/dev/null
+        echo "" >> $LOG_FILE
+    
     done
-    '
 
-    # 4. Compute Analyzing Time
-    echo " > $i Analyzing Time" >> $LOG_FILE
-    yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
-
-    # 5. Clean-up
-    python main.py --remove containers >/dev/null
-    echo "" >> $LOG_FILE
-
+    python main.py --remove all
+    
 done
 
 echo "------------" >> $LOG_FILE
@@ -173,12 +185,8 @@ echo "" >> $LOG_FILE
 echo "Testing 100 containers (10 priv. + 90 default)"
 
 echo "Cleaning up the database..."
-# 1. Clean-up database and remove all containers
+# Clean-up database and remove all containers
 python main.py --remove all
-
-# 2. Initialize the database (configuration + vulnerabilities)
-echo "Initializing the database..."
-python main.py --run ciao >/dev/null
 
 echo "Execution Times for 100 containers (10 priv. and 90 default)" >> $LOG_FILE
 echo "" >> $LOG_FILE
@@ -186,11 +194,14 @@ echo "" >> $LOG_FILE
 echo "Computing Loading and Analyzing times for 100 containers..."
 
 for i in $(seq 1 $TOTAL_RUNS); do 
+
+    python main.py --run ciao >/dev/null
+
     for i in $(seq 1 $TOTAL_RUNS); do 
 
         echo " > Iteration $i"
 
-        # 3. Computing Loading Time
+        # Computing Loading Time
         echo " > $i Loading Time" >> $LOG_FILE
         /usr/bin/time -a -o $LOG_FILE -p sh -c '
         for i in $(seq 1 10); do 
@@ -201,11 +212,11 @@ for i in $(seq 1 $TOTAL_RUNS); do
         done
         '
 
-        # 4. Compute Analyzing Time
+        # Compute Analyzing Time
         echo " > $i Analyzing Time" >> $LOG_FILE
         yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
 
-        # 5. Clean-up
+        # Clean-up
         python main.py --remove containers >/dev/null
         echo "" >> $LOG_FILE
 
@@ -224,12 +235,8 @@ echo "" >> $LOG_FILE
 echo "Testing 100 mixed containers "
 
 echo "Cleaning up the database..."
-# 1. Clean-up database and remove all containers
+# Clean-up database and remove all containers
 python main.py --remove all
-
-# 2. Initialize the database (configuration + vulnerabilities)
-echo "Initializing the database..."
-python main.py --run ciao >/dev/null
 
 echo "Execution Times for 100 mixed containers" >> $LOG_FILE
 echo "" >> $LOG_FILE
@@ -240,6 +247,9 @@ echo "Computing Loading and Analyzing times for 100 containers..."
 # sudo apparmor_parser -r -W ./files/Apparmor/docker-nginx
 
 for i in $(seq 1 $TOTAL_RUNS); do 
+
+    python main.py --run ciao >/dev/null
+
     for i in $(seq 1 $TOTAL_RUNS); do 
 
         echo " > Iteration $i"
@@ -252,7 +262,7 @@ for i in $(seq 1 $TOTAL_RUNS); do
             # https://docs.docker.com/engine/security/apparmor/
         # 5. Container mixed 5 uses 3 common recommendations suggested by the Docker CIS Benchmark (i.e., limited cpus and memory)
 
-        # 3. Computing Loading Time
+        # Computing Loading Time
         echo " > $i Loading Time" >> $LOG_FILE
         /usr/bin/time -a -o $LOG_FILE -p sh -c '
         for i in $(seq 1 20); do 
@@ -264,11 +274,11 @@ for i in $(seq 1 $TOTAL_RUNS); do
         done
         '
 
-        # 4. Compute Analyzing Time
+        # Compute Analyzing Time
         echo " > $i Analyzing Time" >> $LOG_FILE
         yes 2>/dev/null | /usr/bin/time -a -o $LOG_FILE -p python main.py --analyze >/dev/null
 
-        # 5. Clean-up
+        # Clean-up
         python main.py --remove containers >/dev/null
         echo "" >> $LOG_FILE
 
